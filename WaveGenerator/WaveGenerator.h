e14bf8c85c41632b1acb9756dca6e7ddfa2c3c39
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2021-12-27 19:28:31 +0900

    add square wave

--- WaveGenerator/WaveGenerator.h
+++ WaveGenerator/WaveGenerator.h
@@ -23,26 +23,37 @@ class WaveGenerator {
     void SetAmplitude(double amplitude) { //振幅
       this->amplitude = amplitude;
     }
+
     void SetCycle(double cycle) { //波長
       this->cycle = cycle;
     }
+
     void SetFrequency(double Hz) { //周波数(Hz)
       cycle = (double)1000 / Hz;
     }
+
     void SetAngularFrequency(double rad_S) { //角周波数(ω = rad / s)
       cycle = (double)1000 / rad_S;
     }
 
+
     double GetSine(double xPhase = 0, double yPhase = 0) {//(ms)(ms)
-      return amplitude * sin(((time - lastTime - xPhase) * 2 / cycle) * PI) / 2 + yPhase;
+      return amplitude * (sin((time - lastTime - xPhase) * 2 / cycle * PI) + 1) / 2 + yPhase;
     }
+
+    double GetSquare(double xPhase = 0, double yPhase = 0, byte dutyRatio_Per = 50) {
+      return !(fmod(time - lastTime + (cycle - xPhase), cycle) > cycle * dutyRatio_Per / 100) * amplitude + yPhase;
+    }
+
     double GetSawtooth(double xPhase = 0, double yPhase = 0) {
       return amplitude * fmod(time - lastTime + (cycle - xPhase), cycle) / cycle + yPhase;
     }
+
     double GetTriangle(double xPhase = 0, double yPhase = 0) {
       double toMacro = GetSawtooth(xPhase) * 2 - amplitude;
       return abs(toMacro) + yPhase;
     }
+
     double GetTrapezoidal(double xPhase = 0, double yPhase = 0, double truncation = 0) {
       double toMacro = GetTriangle(xPhase);
       return constrain(toMacro, -truncation, truncation) * amplitude / truncation + yPhase;
--- WaveGenerator/WaveGenerator.ino
+++ WaveGenerator/WaveGenerator.ino
@@ -1,5 +1,7 @@
+#define AMPLITUDE 50
+
 #include "WaveGenerator.h"
-WaveGenerator wave(3000, 50);
+WaveGenerator wave(1000, AMPLITUDE);
 
 #include <Adafruit_NeoPixel.h>
 Adafruit_NeoPixel serialLed(20, 13, NEO_RGB + NEO_KHZ800);
@@ -10,12 +12,12 @@ void setup() {
 
 void loop() {
   wave.Update();
-  int cut = 150 / 3;
+  int cut = AMPLITUDE * 2 / 3;
   for (int i = 0; i < 20; i++)
-    serialLed.setPixelColor(i, wave.GetTrapezoidalWave(0, 0, cut), wave.GetTrapezoidalWave(1000, 0, cut), wave.GetTrapezoidalWave(2000, 0, cut));
-  Serial.print(wave.GetTrapezoidalWave(0, 0, cut));   Serial.print("\t");
-  Serial.print(wave.GetTrapezoidalWave(1000, 0, cut));   Serial.print("\t");
-  Serial.print(wave.GetTrapezoidalWave(2000, 0, cut)); Serial.print("\n");
+    serialLed.setPixelColor(i, wave.GetTrapezoidal(0, 0, cut), wave.GetTrapezoidal(1000, 0, cut), wave.GetTrapezoidal(2000, 0, cut));
+  Serial.print(wave.GetSquare(  0, 0, 30));    Serial.print("\n");
+  Serial.print(wave.GetSquare(100, 0, 10)); Serial.print("\t");
+  Serial.print(wave.GetSquare(200, 0, 10)); Serial.print("\n");
   delay(5);
   serialLed.show();
 }
